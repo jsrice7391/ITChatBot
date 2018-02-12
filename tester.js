19ec8769c65823c8137d5d93cbcb67b04a38b9b0
Justin Rice <jsrice7391@gmail.com>
2018-02-12 15:11:32 -0500

    First Commit

---
+++ tester.js
@@ -0,0 +1,37 @@
+// This loads the environment variables from the .env file
+require('dotenv-extended').load();
+
+var builder = require('botbuilder');
+var restify = require('restify');
+
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+// Create connector and listen for messages
+var connector = new builder.ChatConnector({
+    // appId: process.env.MICROSOFT_APP_ID,
+    // appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+server.post('/api/messages', connector.listen());
+
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
+});
+
+// You can provide your own model by specifing the 'LUIS_MODEL_URL' environment variable
+// This Url can be obtained by uploading or creating your model from the LUIS portal: https://www.luis.ai/
+var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
+bot.recognizer(recognizer);
+
+
+bot.dialog("Greeting", function(session) {
+    session.send("Hello! Welcome to the biogen chat bot!")
+   
+  })
+  .triggerAction({
+    matches: "Greeting"
+  })
+
