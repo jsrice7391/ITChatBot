ee4a78c273c136155b74c353a008ca75d91022e5
Justin Rice <jsrice7391@gmail.com>
2018-03-01 13:11:31 -0500

    The QNA bot matches

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 // This loads the environment variables from the .env file
 require('dotenv-extended').load();
-
 var builder = require('botbuilder');
 var restify = require('restify');
 var cog = require("botbuilder-cognitiveservices");
@@ -8,6 +7,7 @@ var cog = require("botbuilder-cognitiveservices");
 
 // Setup Restify Server
 var server = restify.createServer();
+// The actual start of the server
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
 });
@@ -20,14 +20,13 @@ var connector = new builder.ChatConnector({
 // The HTTP route that we can listen on for the servers incoming requests
 server.post('/api/messages', connector.listen());
 
-var bot = new builder.UniversalBot(connector, function (session) {
-    session.send('Sorry, I did not understand \'%s\'. Type \'help\' if you need assistance.', session.message.text);
-});
+// We are establishing the Universal bot instance here. The bot will take an arguemnt of session.
+var bot = new builder.UniversalBot(connector)
 
-// You can provide your own model by specifing the 'LUIS_MODEL_URL' environment variable
-// This Url can be obtained by uploading or creating your model from the LUIS portal: https://www.luis.ai/
+
+// Establish the LUIS connection through the API which is hiden
 var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
-bot.recognizer(recognizer);
+
 
 // Make the recognizer For the QnA service to be used.
 var qnaRecognizer = new cog.QnAMakerRecognizer({
@@ -37,113 +36,26 @@ var qnaRecognizer = new cog.QnAMakerRecognizer({
 
 
 
-// These are the following dialogs. The first argument is the name of the intent that LUIS will match, the callback is what 
-// Luis will do afote he has it completed
-bot.dialog("Greeting", function(session) {
-    session.send("Hello! Welcome to the biogen chat bot! Feel Free to ask me anything regarding Office") 
-  })
-  .triggerAction({
-    matches: "Greeting"
-  })
+var intentsDialog = new builder.IntentDialog({ recognizers: [recognizer, qnaRecognizer] });
+bot.dialog("/", intentsDialog)
 
 
-bot.dialog('Common', function(session) {
-    // The query is set to the session test that comes back
-    var query = session.message.text;   
-    //  We take the Recognizer and pass it the qeury that came within the text;
-    cog.QnAMakerRecognizer.recognize(query, `https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/${process.env.QAID}/generateAnswer`, process.env.SUB_KEY, 1, 'intentName', (error, results) => {
+intentsDialog.matches("qna", (session, args, next) => {
+    var answerEntity = builder.EntityRecognizer.findEntity(args.entities, "answer");
+    session.send(answerEntity.entity);
 
-    // If something goes wrong within the Query being returned, send a user the response
-      if(err){
-        session.send("Gah! I could not find an answer to your question")
-      }else{
-             // Send the first answer that comes within the query
-             session.send(results.answers[0].answer);
-           }
-
-    })    
-}).triggerAction({
-    matches: 'Common'
-});
-
-
-// This is for the Create Ticket
-bot.dialog("CreateTicket", function(session) {
-    session.send("So you want to create a ticket?");
-    // session.beginDialog("/getTicketInfo")
-  })
-  .triggerAction({
-    matches: "CreateTicket"
-  });
-
-// This is for the Outlook inbox
-bot.dialog("Calendar", function(session) {
-    session.send("Managing time is important. Whast kind Of calendar issue ars you having?");
-  })
-  .triggerAction({
-    matches: "Calendar"
-  });
-  
-
-  // This is for the Create Ticket
-bot.dialog("DistributionList", function(session) {
-    session.send("That seems Like somthing we can take care of!");
-  })
-  .triggerAction({
-    matches: "DistirbutionList"
-  });
-  
-
-  // This is for the Create Ticket
-bot.dialog("Groups", function(session) {
-    session.send("Groups are a great way to...");
-  })
-  .triggerAction({
-    matches: "Groups"
-  });
-  
-  // This is for the Create Ticket
-bot.dialog("OneDrive", function(session) {
-    session.send("One Drive is a great platform. Lets Get started");
-  })
-  .triggerAction({
-    matches: "OneDrive"
-  });
-  
-
-  // This is for the Create Ticket
-bot.dialog("Outlook", function(session) {
-    session.send("An Outlook question? Sounds good!");
-  })
-  .triggerAction({
-    matches: "Outlook"
-  });
-  
-
-  // This is for the Create Ticket
-bot.dialog("SharedMailbox", function(session) {
-    session.send("What Kind of Shared mailbox?");
-  })
-  .triggerAction({
-    matches: "SharedMailbox"
-  });
-  
-
-  // This is for the Create Ticket
-bot.dialog("VideoStreams", function(session) {
-    session.send("A video strem!?!?");
-  })
-  .triggerAction({
-    matches: "VideoStreams"
-  });
-  
-
-bot.dialog("None", function(session){
-  session.send("I am sorry, I do not know what to say")
-}).triggerAction({
-  matches: "None"
 })
-    
+
+
+
+intentsDialog.onDefault([
+  function(session){
+    session.send("Sorry I do not see anything in the prebuilt")
+  }
+
+]);
+
+
 
 
 
